87f5a56eb5984da62c9b1f56268ce2a06201b39f
Ian Kirk <iankirk24@gmail.com>
2023-09-18 14:24:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// 1. Global Constants 
+//      - var PI = 3.14 
+//      - PI 42 ; Stop me from doing this! 
+//      Answer: Const PI = 3.14; 
+// 2. Whats the difference between var and let?
+//      Answer: Var is scoped to the whole variable and can be reassigned and redeclared. Var is also hoisted to the top of the scope it is defined in. Let can only be reassigned but not redeclared. Let also only exists inside of the code-block it is assigned in. 
+// 3. What's the difference between var and const?
+//      Answer: Var can be reassigned and redeclared but const can't be reassigned or redeclared. Var is hoisted to the top of the scope while const creates a block-scope. 
+// 4. Whats the difference between let and const?
+//      Answer: Let can be reassigned but const cannot be reassigned. 
+// 5. What is hoisting? 
+//      Answer: Variables can be lifted to the top of the scope they are in and can be called before they are assigned in the scope.
